213489956ff82c43d3b1290ab619c26db20039ae
jon-erikklint <jonerik.klint@gmail.com>
2017-05-22 14:15:06 +0300

    ylimääräiset console.logit pois

--- src/sprites/ViewTile.js
+++ src/sprites/ViewTile.js
@@ -3,14 +3,10 @@ import ModelTile from '../models/ModelTile'
 
 export default class {
     constructor ({ game, x, y, modelTile}) {
-        console.log(game)
-
         this.game = game
         this.x = x
         this.y = y
 
-        console.log(modelTile)
-        console.log(modelTile.tileType)
         this.nature = game.add.sprite(x, y, modelTile.tileType.asset)
     }
 }
