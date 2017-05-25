89205affcc651bb9ee06b59c46c1d162a19b2459
Ilja Häkkinen <ilja.hakkinen@helsinki.fi>
2017-05-25 19:03:55 +0300

    hashmap given to the menu constructor

--- src/models/Menu.js
+++ src/models/Menu.js
@@ -1,22 +1,25 @@
 export default class Menu {
-    constructor (game, menuViewWidth) {
-        this.menuViewGroup = game.add.group()
-        this.title = 'Choose a tile first'
-        this.menuActions = []
+  constructor ({ game, menuViewWidth }) {
+    this.menuViewGroup = game.add.group()
+    this.title = 'Choose a tile first'
+    this.menuActions = []
 
-        createBackground(this.menuViewGroup, game.world.width - menuViewWidth, 0, menuViewWidth, game.world.height, 0x993333)
-        this.menuViewGroup.fixedToCamera = true
-    }
+    createBackground(
+      this.menuViewGroup, game.world.width - menuViewWidth, 0, menuViewWidth, game.world.height, 0x993333)
+    this.menuViewGroup.fixedToCamera = true
+  }
 
-
+  update () {
+    //
+  }
 }
 
 function createBackground (viewGroup, leftX, leftY, width, height, color) {
-    var background = game.make.graphics()
+  var background = game.make.graphics()
 
-    background.beginFill(color, 1)
-    background.drawRect(leftX, leftY, width, height)
-    background.endFill()
+  background.beginFill(color, 1)
+  background.drawRect(leftX, leftY, width, height)
+  background.endFill()
 
-    viewGroup.add(background)
+  viewGroup.add(background)
 }
