6965f7d7af63a85401b3374842d7478ecf796b2a
Miia <miia.ramo@helsinki.fi>
2017-05-31 23:11:05 +0300

    Player class tested

--- src/game/Player.js
+++ src/game/Player.js
@@ -1,14 +1,15 @@
 // The class Player keeps track of players points
-class Player {
-    // initially player has 0 points
-    constructor () {
-        this.points = 0
-    }
-    // adds points given as a parameter to players existing points
-    addPoints (points) {
-        this.points += points
-    } 
-    getPoints (points) {
-        return this.points
-    } 
+export default class {
+  // initially player has 0 points
+  constructor () {
+    this.points = 0
+  }
+  // adds points given as a parameter to players existing points
+  addPoints (points) {
+    this.points += points
+  }
+  // returns players current points
+  getPoints (points) {
+    return this.points
+  }
 }
--- test/game/PlayerTest.js
+++ test/game/PlayerTest.js
@@ -1,6 +1,14 @@
-const assert = require("assert")
-import Player from '../../src/game/Player.js'
+import Player from '../../src/game/Player.js';
+const assert = require("assert");
 
 describe('Player tests', function(){
-  
+  it('Player should have 0 points in the beginning', function(){
+    var p = new Player();
+    assert.equal(0, p.getPoints());
+  })
+  it('Points are added correctly', function(){
+    var p = new Player();
+    p.addPoints(5);
+    assert.equal(5, p.getPoints());
+	})
 })
