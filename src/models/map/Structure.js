2f9ac65f3c347d81ab412611e2b9a6f21af416c5
jon-erikklint <jonerik.klint@gmail.com>
2017-06-06 12:48:08 +0300

    structure

---
+++ src/models/map/Structure.js
@@ -0,0 +1,12 @@
+import Phaser from 'phaser'
+
+export default class Structure{
+  constructor ({tile, structureType}) {
+    this.tile = tile
+    this.structureType = structureType
+  }
+
+  asset(){
+    return this.structureType.asset
+  }
+}
