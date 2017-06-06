0ad0127f9256aa57b30ccc6b4a7baabaeb04fba1
jon-erikklint <jonerik.klint@gmail.com>
2017-06-06 11:24:56 +0300

    viewtilen muutos

--- src/view/map/ViewTile.js
+++ src/view/map/ViewTile.js
@@ -1,8 +1,27 @@
 import Phaser from 'phaser'
 
-export default class extends Phaser.Sprite {
+export default class ViewTile{
   constructor ({game, x, y, modelTile}) {
-    super(game, x, y, modelTile.tileType.asset)
+    this.game = game
     this.modelTile = modelTile
+
+    this.tileSprite = game.add.sprite(x, y, modelTile.tileType.asset)
+
+    if(modelTile.structure != null){
+      makeStructureSprite()
+    }
+  }
+
+  update(){
+    if(this.modelTile.structure != null && this.structureSprite == null){
+      makeStructureSprite()
+    }else if(this.modelTile.structure == null && this.structureSprte != null){
+      this.structureSprite.destroy()
+      this.structureSprite = null
+    }
+  }
+
+  makeStructureSprite(){
+    this.structureSprte = this.tileSprite.addChild(game.make.sprite(0, 0, modelTile.structure.asset))
   }
 }
