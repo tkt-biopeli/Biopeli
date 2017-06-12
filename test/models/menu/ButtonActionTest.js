28faadef79a74cdd51d52688fbd3ffad028c13df
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2017-06-12 14:51:07 +0300

    button action testattu

---
+++ test/models/menu/ButtonActionTest.js
@@ -0,0 +1,17 @@
+const assert = require('assert')
+const sinon = require('sinon')
+import ButtonAction from '../../../src/models/menu/ButtonAction'
+
+describe('ButtonAction tests', () =>{
+
+  it('Constructor test', () =>{
+    var testBA = new ButtonAction({
+        name: 'name',
+        functionToCall: 'func',
+        context: 'con'
+    })
+    assert.equal('name', testBA.name)
+    assert.equal('func', testBA.functionToCall)
+    assert.equal('con', testBA.context)
+  })
+})
