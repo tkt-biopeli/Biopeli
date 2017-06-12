fed0cc71000a3a92ec9ec4c754e8f8b0e4b82abb
jon-erikklint <jonerik.klint@gmail.com>
2017-06-12 15:11:11 +0300

    rakennustiedot näkyvät

--- src/view/menu/MenuView.js
+++ src/view/menu/MenuView.js
@@ -35,14 +35,84 @@ export default class MenuView {
       return
     }
 
-    var menuFunctions = [this.createTileInformation, this.createButtons]
-
-    console.log(menuFunctions.length)
+    var menuFunctions = [
+      this.createTileInformation,
+      this.createStructureInformation,
+      this.createButtons]
 
     for(var i = 0 ; i < menuFunctions.length ; i++){
-      menuFunctions[i].call(this)
-      this.addSectionPadding()
+      if(menuFunctions[i].call(this)) this.addSectionPadding()
     }
+  }
+
+  createBackground () {
+    this.menuViewGroup.create(this.leftBorderCoordinate, 0, 'menuBg')
+  }
+
+  createTileInformation () {
+    var tile = this.menu.selectedTile
+
+    this.createText('Ground type: '+tile.tileType.name, 16)
+    this.addLinePadding()
+    this.createText('X: '+tile.x+", Y: "+tile.y, 16)
+
+    return true
+  }
+
+  createStructureInformation () {
+    var structure = this.menu.selectedTile.structure
+
+    if (structure == null) {
+      return false
+    }
+
+    this.createText('Structure: '+structure.structureType.name, 16)
+  }
+
+  createButtons () {
+    for (var i = 0, len = this.buttonActions.length; i < len; i++) {
+      this.createButton(this.buttonActions[i])
+      this.addLinePadding()
+    }
+
+    return true
+  }
+
+  createButton (buttonAction) {
+    var resetDecorator = new ResetDecorator({action: buttonAction, menu: this.menu})
+
+    var button = new LabeledButton({
+      game: this.game,
+      viewGroup: this.menuViewGroup,
+      label: buttonAction.name,
+      x: this.leftBorderCoordinate + this.leftPadding,
+      y: this.drawHeight,
+      callback: resetDecorator.act,
+      context: resetDecorator,
+      buttonWidth: this.buttonWidth,
+      buttonHeight: this.buttonHeight
+    })
+
+    this.addPadding(this.buttonHeight)
+  }
+
+  createText (text, fontSize) {
+    var text = new Text({
+      game: this.game,
+      viewGroup: this.menuViewGroup,
+      text: text,
+      fontSize: fontSize,
+      x: this.leftBorderCoordinate + this.leftPadding,
+      y: this.drawHeight
+    })
+    this.addPadding(fontSize)
+
+    return text
+  }
+
+  setButtonActions (buttonActions) {
+    this.buttonActions = buttonActions
+    this.redraw()
   }
 
   addPadding (amount) {
@@ -55,59 +125,5 @@ export default class MenuView {
 
   addSectionPadding () {
     this.addPadding(this.sectionPadding)
-  }
-
-  createBackground () {
-    this.menuViewGroup.create(this.leftBorderCoordinate, 0, 'menuBg')
-  }
-
-  createTileInformation () {
-    var tile = this.menu.selectedTile
-
-    this.createText('Ground type: '+tile.tileType.name, 16)
-    this.addPadding(16)
-    this.addLinePadding()
-    this.createText('X: '+tile.x+", Y: "+tile.y, 16)
-    this.addPadding(16)
-  }
-
-  createButtons () {
-    for (var i = 0, len = this.buttonActions.length; i < len; i++) {
-      this.createButton(this.buttonActions[i])
-      this.drawHeight += this.buttonHeight
-      this.addLinePadding()
-    }
-  }
-
-  createButton (buttonAction) {
-    var resetDecorator = new ResetDecorator({action: buttonAction, menu: this.menu})
-
-    return new LabeledButton({
-      game: this.game,
-      viewGroup: this.menuViewGroup,
-      label: buttonAction.name,
-      x: this.leftBorderCoordinate + this.leftPadding,
-      y: this.drawHeight,
-      callback: resetDecorator.act,
-      context: resetDecorator,
-      buttonWidth: this.buttonWidth,
-      buttonHeight: this.buttonHeight
-    })
-  }
-
-  createText (text, fontSize) {
-    return new Text({
-      game: this.game,
-      viewGroup: this.menuViewGroup,
-      text: text,
-      fontSize: fontSize,
-      x: this.leftBorderCoordinate + this.leftPadding,
-      y: this.drawHeight
-    })
-  }
-
-  setButtonActions (buttonActions) {
-    this.buttonActions = buttonActions
-    this.redraw()
   }
 }
