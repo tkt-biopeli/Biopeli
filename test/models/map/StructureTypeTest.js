5c849b29abc86996dee523d5d6e797475bcbcbe1
Jarkko Karttunen <jarkko.karttunen1@gmail.com>
2017-06-19 16:00:02 +0300

    tests

--- test/models/map/StructureTypeTest.js
+++ test/models/map/StructureTypeTest.js
@@ -1,4 +1,5 @@
 const assert = require("assert")
+
 import StructureType from '../../../src/models/map/StructureType'
 
 describe('StructureType tests', () =>{
@@ -8,10 +9,10 @@ describe('StructureType tests', () =>{
     assert.equal(2, Object.keys(structures).length)
   })
 
-  it('', () => {
-
+  it('Create update function is never undefined', () => {
+    var types = StructureType()
+    for(var type in types){
+      assert.equal('function', typeof types[type].createUpdateFn)
+    }
   })  
-  it('', () => {
-    
-  })
 })
