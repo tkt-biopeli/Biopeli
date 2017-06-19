7b6545ca24d2839809f96783530c80d371332939
Ilja Häkkinen <ilja.hakkinen@helsinki.fi>
2017-06-19 14:50:01 +0300

    productionInput added, calculateProductionEfficiency returns a constant value.

--- test/models/map/StructureTest.js
+++ test/models/map/StructureTest.js
@@ -2,18 +2,30 @@ const assert = require("assert")
 import Structure from '../../../src/models/map/Structure'
 
 describe('Structure tests', () =>{
+  
+  var structure
+  
+  before(function () {
+    structure = new Structure({tile: 0, name: "Riston rustholli", size: 64, structureType: 1})
+  })
+  
   it('Constructor works', () =>{
-    var structure = new Structure({tile: 0, name: "Riston rustholli", size: 64, structureType: 1})
-
     assert.equal(0, structure.tile)
     assert.equal(1, structure.structureType)
     assert.equal("Riston rustholli", structure.name)
     assert.equal(64, structure.size)
+    assert.equal(0, structure.productionInput)
   })
 
   it('asset-shortcut works', () =>{
-    var structure = new Structure({tile: 0, name: "A", size: 1, structureType: {asset: "sd"}})
-
+    structure = new Structure({tile: 0, name: "Riston rustholli", size: 64, structureType: {asset: "sd"}})
     assert.equal("sd", structure.asset())
+  })
+  
+  it('calculateProductionEfficiency returns correct value', () =>{
+    // calculate manually (use mock random if random factors are used)
+    var expected = 785
+    
+    assert.equal(expected, structure.calculateProductionEfficiency())
   })
 })
