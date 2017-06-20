438f9f670f74545e031fd08c1781cd5532673651
jon-erikklint <jonerik.klint@gmail.com>
2017-06-20 17:37:52 +0300

    ruudu valinta integraatio toimii

--- test/integration/TileSelectionTest.js
+++ test/integration/TileSelectionTest.js
@@ -28,7 +28,7 @@ describe('Integration test: Tile selection shows correctly in Menu', () =>{
     gameStateChecker.checkSelectedTile(0, 0)
     gameStateChecker.checkButtonAmountInMenu(0)  
     gameStateChecker.checkIfTextsExist('Ground type: grass', 'X: 0, Y: 0', 'Structure: farm', 
-      'Founding year: 1999', 'Size: joku koko', 'Production input: 0', 'Production per time: ' ) 
+      'Founding year: 1999', 'Size: 100', 'Production input: 0', 'Production per time: ' ) 
   })
 
 })
