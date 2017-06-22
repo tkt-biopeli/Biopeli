60fcb4cd60fe472c745521bf989403f6af577c2e
jon-erikklint <jonerik.klint@gmail.com>
2017-06-22 13:22:17 +0300

    sd

--- src/models/topbar/TopBarControllerDemo.js
+++ src/models/topbar/TopBarControllerDemo.js
@@ -3,14 +3,13 @@ export default class TopBarControllerDemo {
     this.player = player
     this.topBar = topBar
     this.topBarView = topBarView
-    this.timer = 1
   }
 
   onTimer (timerEvent) {
     this.player.addPoints(10)
     this.topBar.setValueOf('score', this.player.getPoints())    
     let r = Math.floor(Math.random() * 100)
-    this.topBar.setValueOf('time', this.timer++)
+    this.topBar.setValueOf('time', timerEvent.toString())
     this.topBar.setValueOf('turnip', r)
     this.topBarView.update()    
   }
