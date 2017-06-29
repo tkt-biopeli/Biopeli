9f421d0f4dad4bbe095dc5e927abb6a9127b6db9
jon-erikklint <jonerik.klint@gmail.com>
2017-06-29 17:03:46 +0300

    maplistener testit toimii

--- test/view/MapListenerTest.js
+++ test/view/MapListenerTest.js
@@ -7,17 +7,15 @@ describe('MapListener tests', () => {
   var game
   var map
   var menuOptionCreator
-  var menu
+  var menuController
   var ml
 
 
   beforeEach(() => {
 
-    menu = {
-      menuView: {
-        leftBorderCoordinate: 0
-      },
-      selectedTile: undefined,
+    menuController = {
+      menuView: { layout: {menuRect: {x: 0}}},
+      selectedTile: null, 
       reset: function () { },
       chooseTile: function () { }
     }
@@ -39,23 +37,23 @@ describe('MapListener tests', () => {
       getActions: function () { }
     }
 
-    ml = new MapListener({ game: game, map: map, menuOptionCreator: menuOptionCreator, menu: menu })
+    ml = new MapListener({ game: game, map: map, menuOptionCreator: menuOptionCreator, menuController: menuController })
 
   })
 
   it('Constructor works', () => {
-    ml = new MapListener({ game: game, map: map, menuOptionCreator: "jasd", menu: menu })
+    ml = new MapListener({ game: game, map: map, menuOptionCreator: "jasd", menuController: menuController })
     assert.equal(ml.game, game)
     assert.equal(ml.map, map)
     assert.equal(ml.menuOptionCreator, "jasd")
-    assert.equal(ml.menuController, menu)
+    assert.equal(ml.menuController, menuController)
   })
 
   it('Pointer event in map area is recognized', () => {
     var pointerEvent = { x: 70, y: 50 }
-    menu.menuView.menuBorderCoordinate = 60
+    menuController.menuView.layout.menuRect.x = 60
     assert.equal(ml.pointerInMapArea(pointerEvent), false)
-    menu.menuView.menuBorderCoordinate = 70
+    menuController.menuView.layout.menuRect.x = 70
     assert.equal(ml.pointerInMapArea(pointerEvent), true)
   })
 
@@ -64,15 +62,15 @@ describe('MapListener tests', () => {
   })
 
   it('Selected tile is recognized', () => {
-    menu.selectedTile = "tile"
-    var mock = sinon.mock(menu)
+    menuController.selectedTile = "tile"
+    var mock = sinon.mock(menuController)
     mock.expects("reset").once()
     assert.equal(ml.validTile("tile"), false)
     mock.verify()
   })
 
   it('New selected tile is recognized', () => {
-    menu.selectedTile = "tile"
+    menuController.selectedTile = "tile"
     assert.equal(ml.validTile("anotherTile"), true)
   })
 
@@ -87,7 +85,7 @@ describe('MapListener tests', () => {
   })
 
   it('Menu update functions are called', () => {
-    var m1 = sinon.mock(menu)
+    var m1 = sinon.mock(menuController)
     var m2 = sinon.mock(menuOptionCreator)
     m1.expects("chooseTile").once()
     m2.expects("getActions").once()
@@ -98,15 +96,15 @@ describe('MapListener tests', () => {
 
   it('Update with pointer in map area calls correct functions', () => {
     var pointerEvent = { x: 70, y: 50 }
-    menu.selectedTile = "tile"
-    menu.menuView.menuBorderCoordinate = 80
+    menuController.selectedTile = "tile"
+    menuController.menuView.layout.menuRect.x = 80
     game.camera.x = 0
     game.camera.y = 0
 
     var stub = sinon.stub(ml, "getTileFromMap")
     stub.returns("justATile")
 
-    var mockMenu = sinon.mock(menu)
+    var mockMenu = sinon.mock(menuController)
     mockMenu.expects("chooseTile").once()
 
     var mockMenuOptions = sinon.mock(menuOptionCreator)
@@ -120,8 +118,8 @@ describe('MapListener tests', () => {
 
   it('Update with pointer outside map area doesnt call any functions', () => {
     var pointerEvent = { x: 70, y: 50 }
-    menu.menuView.leftBorderCoordinate = 60
-    var mockMenu = sinon.mock(menu)
+    menuController.menuView.layout.menuRect.x = 60
+    var mockMenu = sinon.mock(menuController)
     mockMenu.expects("chooseTile").never()
 
     var mockMenuOptions = sinon.mock(menuOptionCreator)
@@ -134,12 +132,12 @@ describe('MapListener tests', () => {
 
   it('Unvalid selection in map are doesnt call any functions', () => {
     var pointerEvent = { x: 70, y: 50 }
-    menu.menuView.leftBorderCoordinate = 80
+    menuController.menuView.leftBorderCoordinate = 80
 
     var stub = sinon.stub(ml, "getTileFromMap")
     stub.returns(undefined)
 
-    var mockMenu = sinon.mock(menu)
+    var mockMenu = sinon.mock(menuController)
     mockMenu.expects("chooseTile").never()
 
     var mockMenuOptions = sinon.mock(menuOptionCreator)
