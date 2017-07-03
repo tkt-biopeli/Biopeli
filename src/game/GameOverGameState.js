bd57c1d18dcab152411acaea0f55ab7131190d27
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2017-07-03 12:43:25 +0300

    game over ruutu melkein valmis

--- src/game/GameOverGameState.js
+++ src/game/GameOverGameState.js
@@ -1,19 +1,20 @@
 /**
  * Description goes here
  */
-export default class GameState {
+export default class GameOverGameState {
   /**
    * @param {Phaser.Game} param.state - Current state
    */
   constructor ({ state }) {
     this.state = state
+    this.state.add.sprite(100, 200, 'gameover')
   }
 
   /**
    * Description goes here
    */
   update () {
-    this.state.add.sprite(0, 0, 'gameover')
+   
   }
 
 }
