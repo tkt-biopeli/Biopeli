ebeaaa6ca583816139be595d33cfe1521cc30b9b
jon-erikklint <jonerik.klint@gmail.com>
2017-07-04 18:01:10 +0300

    paljon nimiä

--- src/models/misc/CityNameGenerator.js
+++ src/models/misc/CityNameGenerator.js
@@ -2,6 +2,42 @@ import cityNames from './CityNameList'
 
 export default class CityNameGenerator {
   generateName () {
-    return 'mTechVille'
+    var first = this.getRandomId()
+    var second = this.getRandomId()
+    if(second == first){
+      second ++
+      if(second == cityNames.length){
+        second = 0
+      }
+    }
+
+    var name = this.getStart(first) + this.getEnd(second, first)
+
+    return name
+  }
+
+  getStart (id) {
+    var no = cityNames[id]
+
+    if(no.full != null){
+      return no.name.slice(0, no.full)
+    }
+
+    return no.name.slice(0, no.point)
+  }
+
+  getEnd (id, first) {
+    var no = cityNames[id]
+
+    if(cityNames[first].full != null){
+      return no.name
+    }
+
+    return no.name.slice(no.point)
+  }
+
+  getRandomId(){
+    var r = Math.random()
+    return Math.floor(r * (cityNames.length))
   }
 }
