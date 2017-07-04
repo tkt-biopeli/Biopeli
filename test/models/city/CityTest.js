92d05311588dfdd52336a0c59a2e804778253534
Jarkko Karttunen <jarkko.karttunen1@gmail.com>
2017-07-04 14:01:08 +0300

    tests

--- test/models/city/CityTest.js
+++ test/models/city/CityTest.js
@@ -1,13 +1,19 @@
 import City from '../../../src/models/city/City'
 import config from '../../../src/config'
+import DemandFunction from '../../../src/models/city/DemandFunction'
 const assert = require('assert')
 
 describe('City tests', () => {
 
-  var city
+  var city, demandFn
 
   before(() => {
     city = new City({ name: 'test' })
+    demandFn = new DemandFunction({
+      city: city, 
+      popularityPct: 10, 
+      slope:10
+    })
   })
 
   it('Constructor works', () => {
@@ -41,5 +47,11 @@ describe('City tests', () => {
 
   it('City pays money to player according to satisfaction level', () => {
     // test here
+  })
+
+  it('Buy turnips returns a valid number', () => {
+	  var ret = city.buyTurnips(10)
+    assert(typeof ret == 'number')
+    assert(ret != NaN)
   })
 })
