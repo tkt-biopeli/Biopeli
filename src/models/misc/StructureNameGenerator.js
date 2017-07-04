4f56a73cdfb5f057a7bc941ee98e34bd30fc7253
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2017-07-04 18:44:12 +0300

    hauskoja nimiä rakennuksille

--- src/models/misc/StructureNameGenerator.js
+++ src/models/misc/StructureNameGenerator.js
@@ -9,18 +9,53 @@ export default class StructureNameGenerator {
       'Kyöstin',
       'Jaakon'
     ]
-    this.adjectives = [
+    this.frontAdjectives = [
+      'Juoppo',
+      'Iso',
+      'Pikku',
+      'Hullu',
+      'Sekopää',
+      'Rauhanmies',
+      'Piippu',
+      'Mustan',
+      'Pihi' 
+    ]
+    this.endAdjectives = [
+      'pieni',
+      'iso',
+      'suuri',
+      'kämänen',
+      'siisti',
+      'puhdas',
+      'laadukas',
+      'kurja',
+      'huonon onnen',
+      'pahan päivän',
+      'surkea',
+      'likainen',
+      'kallis',
+      'ylihinnoiteltu',
+      'luomu'
     ]
   }
   createName (structureType) {
     var type = this.findType(structureType)
-    var name = 'test ' + type
-    return name
+    var name = Math.floor(Math.random() * this.names.length)
+    var frontAdj = Math.floor(Math.random() * this.frontAdjectives.length)
+    var endAdj =  Math.floor(Math.random() * this.endAdjectives.length)
+    var fullName = this.frontAdjectives[frontAdj] + ' ' + this.names[name] + ' ' + this.endAdjectives[endAdj] + ' ' + type
+    return fullName
   }
   findType(structureType) {
-      var type
+      var type = ''
       if (structureType == 'farm') {
           type = 'farmi'
+      }
+      if (structureType == 'dairy farm') {
+        type = 'navetta'
+      }
+      if (structureType == 'berry farm') {
+        type = 'marjatila'
       }
       return type
   }
