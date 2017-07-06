2735e6330e713310857642bf2d15a3e846cbbcce
durak <itapkos@gmail.com>
2017-07-06 10:37:21 +0300

    vuosittainen kysyntä eriytetty

--- src/models/city/DemandFunction.js
+++ src/models/city/DemandFunction.js
@@ -5,22 +5,42 @@ export default class DemandFunction {
     this.slope = slope
   }
 
-  calculate (supply) {
-    let ratio = supply / (this.customers() / 2)
+  weekly (supply) {
+    let demand = this.demandedAmount()
+    this.city.weeklyTurnipDemand = demand / 2
+    this.city.yearlyTurnipDemand += demand / 4
+    return this.calculate(supply, demand, true)
+  }
+
+  yearly (supply) {
+    let demand = this.city.yearlyTurnipDemand * 2
+    let fulfilledAndEarnings = this.calculate(supply, demand)
+    this.city.yearlyTurnipDemand = 0
+    return fulfilledAndEarnings
+  }
+
+  calculate (supply, demand, weekly) {
+    let ratio = weekly ? supply / this.city.weeklyTurnipDemand : supply / this.city.yearlyTurnipDemand
     let fulfilledPct = ratio < 1 ? ratio * 100 : 100
+    let slope = weekly ? this.slope : this.slope * 6
 
     return {
-      percentage: fulfilledPct,
-      earnings: supply * this.price(supply)
+      percentage: Math.floor(fulfilledPct),
+      earnings: Math.floor(supply * this.price(supply, demand, slope))
     }
   }
 
-  price (supply) {
-    let price = (this.customers() - supply) / this.slope
+  price (supply, demand, slope) {
+    let price = (demand - supply) / slope
     return price > 0 ? price : 0
   }
 
   customers () {
     return this.city.population * this.popularityPct / 100
+  }
+
+  demandedAmount () {
+    let rvar = (-0.5 + Math.random()) * 20
+    return (Math.floor(this.city.population * (this.popularityPct + rvar) / 100))
   }
 }
