92b5b6e6ba5cf8396a414e8ac0f37a80debd8134
jon-erikklint <jonerik.klint@gmail.com>
2017-07-11 14:51:41 +0300

    korjausta

--- test/controllers/MenuControllerTest.js
+++ test/controllers/MenuControllerTest.js
@@ -9,17 +9,63 @@ describe('Menu controller tests', () =>{
   var buttonActions
   var tile
 
+  var player, structureFactory, tile2, sType, buildSpy
+
   beforeEach(() => {
     spy = sinon.spy()
     menuView = { setMenu: spy, setButtonActions: function () { } }
-    menu = new Menu({ menuView: menuView })
     buttonActions = []
     tile = 1
+
+    buildSpy = sinon.spy()    
+    sType = {
+      name: 'navetta'
+    }
+    
+    tile2 = {
+      structure: null,
+      tileType: {
+        allowedStructures: [sType]
+      }
+    }
+    player = {}
+    structureFactory = {
+      buildBuilding: buildSpy
+    }
+
+
+    menu = new Menu({ 
+      menuView: menuView,
+      player: player,
+      structureFactory: structureFactory
+     })
   })
 
   it('Constructor test', () => {
     assert.equal(menuView, menu.menuView)
     assert.equal(null, menu.selectedTile)
     assert.equal(0, menu.buttonComponents.length)
+    assert.equal(player, menu.player)
+    assert.equal(structureFactory, menu.structureFactory)
+  })
+  
+  it('getActions returns empty array if tile has structure', () => {
+    tile2.structure = 4
+    assert.equal(0, menu.getActions(tile2).length)
+  })
+  
+  it('getActions calls buttonActionsForTile if tile has no structure', () => {
+    var fnSpy = sinon.spy()
+    menu.buttonActionsForTile = fnSpy
+    menu.getActions(tile2)
+    assert(fnSpy.calledWith(tile2))
+  })
+  
+  it('buttonActionsForTile returns array of button actions', () => {
+    var buttonActions = menu.buttonActionsForTile(tile2)
+    assert.equal('navetta : undefined€', buttonActions[0].name)
+    buttonActions[0].function.call()
+    assert(buildSpy.calledWith(tile2, sType))
+    assert.equal(structureFactory, buttonActions[0].context)
   })
 })
--- test/controllers/MenuOptionCreatorTest.js
+++ test/controllers/MenuOptionCreatorTest.js
@@ -1,4 +1,4 @@
-const assert = require('assert')
+/*const assert = require('assert')
 const sinon = require('sinon')
 import MenuOptionCreator from '../../src/controllers/actioncreation/MenuOptionCreator'
 
@@ -26,28 +26,5 @@ describe('Menu option creator tests', () => {
     menuOptionCreator = new MenuOptionCreator({ player: player, structureFactory: structureFactory })
   })
   
-  it('Constructor test', () => {
-    assert.equal(player, menuOptionCreator.player)
-    assert.equal(structureFactory, menuOptionCreator.structureFactory)
-  })
   
-  it('getActions returns empty array if tile has structure', () => {
-    tile.structure = 4
-    assert.equal(0, menuOptionCreator.getActions(tile).length)
-  })
-  
-  it('getActions calls buttonActionsForTile if tile has no structure', () => {
-    var fnSpy = sinon.spy()
-    menuOptionCreator.buttonActionsForTile = fnSpy
-    menuOptionCreator.getActions(tile)
-    assert(fnSpy.calledWith(tile))
-  })
-  
-  it('buttonActionsForTile returns array of button actions', () => {
-    var buttonActions = menuOptionCreator.buttonActionsForTile(tile)
-    assert.equal('navetta : undefined€', buttonActions[0].name)
-    buttonActions[0].function.call()
-    assert(buildSpy.calledWith(tile, sType))
-    assert.equal(structureFactory, buttonActions[0].context)
-  })
-})
+})*/
