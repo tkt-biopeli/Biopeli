e7014d688a5bf08be8b55bb45f17174e64db0e05
Ilja Häkkinen <ilja.hakkinen@helsinki.fi>
2017-07-24 15:37:48 +0300

    temp commit before merging from master

--- test/models/map/StructureFactoryTest.js
+++ test/models/map/StructureFactoryTest.js
@@ -3,13 +3,20 @@ const sinon = require("sinon")
 import StructureFactory from '../../../src/models/map/structure/StructureFactory'
 
 describe('StructureFactory tests', () => {
-  var sfactory, gameTimer, player, addStructureSpy, map
+  var sfactory, gameTimer, player, addStructureSpy, map, tileFinder, tile, structureType
+  var addStructureSpy, buyLandSpy, createPollutionSpy, calcSizeSpy, createProducerSpy
+  var checkMoneyStub, tilesInRadiusStub
 
   beforeEach(() => {
+    addStructureSpy = sinon.spy()
+    tilesInRadiusStub = sinon.stub()
+
+    tileFinder = {}
+
     map = {
       getTilesInRadius: () => new Map()
     }
-    addStructureSpy = sinon.spy()
+
     player = {
       addStructure: addStructureSpy,
       enoughCashFor: () => true,
@@ -25,43 +32,70 @@ describe('StructureFactory tests', () => {
     sfactory = new StructureFactory({
       gameTimer: gameTimer,
       player: player,
-      map: map
+      map: map,
+      tileFinder: tileFinder
     })
 
     sfactory.namer = {
-      createBuildingName: ()=>'joku nimi',
-      createOwnerName: ()=>'nimi joku'
+      createBuildingName: (jotain) => 'joku rakennus',
+      createOwnerName: () => 'joku omistaja'
     }
+
+    tile = { structure: undefined, flowers: 0 }
+    structureType = { cost: 888, pollution: 564 }
   })
 
   it('Constructor works', () => {
     assert.equal(gameTimer, sfactory.gameTimer)
     assert.equal(player, sfactory.player)
+    assert.equal(map, sfactory.map)
+    assert.equal(tileFinder, sfactory.tileFinder)
   })
+
+  var mockMethodsForBuildBuildingTests = () => {
+    checkMoneyStub = sinon.stub()
+    createProducerSpy = sinon.spy()
+    buyLandSpy = sinon.spy()
+    createPollutionSpy = sinon.spy()
+    calcSizeSpy = sinon.spy()
+    sfactory.checkMoney = checkMoneyStub
+    sfactory.createProducer = createProducerSpy
+    sfactory.buyLandInRadiusForTileOwnership = buyLandSpy
+    sfactory.createInitialPollution = createPollutionSpy
+    sfactory.calculateSizeAndChangeAssets = calcSizeSpy
+  }
 
   it('Build building works', () => {
-    var tile = { structure: {}, flowers: 0 }
-    var structureType = {}
-    var createProducerSpy = sinon.spy()
-    sfactory.createProducer = createProducerSpy
+    mockMethodsForBuildBuildingTests()
+    checkMoneyStub.withArgs(structureType).returns(true)
     sfactory.buildBuilding(tile, structureType)
 
-    assert.equal(tile, tile.structure.tile)
-    assert.equal(0, tile.structure.size)
-    assert.equal(structureType, tile.structure.structureType)
-    assert.equal(7, tile.structure.foundingYear)
+    assert.equal(tile.structure.tile, tile)
+    assert.equal(tile.structure.owner, 'joku omistaja')
+    assert.equal(tile.structure.name, 'joku rakennus')
+    assert.equal(tile.structure.size, 0)
+    assert.equal(tile.structure.structureType, structureType)
+    assert.equal(tile.structure.foundingYear, 7)
     assert(createProducerSpy.calledWith(structureType, tile))
-    assert(addStructureSpy.calledWith(tile.structure))
-  })
-  it('Build building does not do anything if checkMoney returns false', () => {
-    var tile = { structure: undefined, flowers: 0 }
-    var structureType = {}
-    var checkMoneyStub = sinon.stub()
-    checkMoneyStub.withArgs(structureType).returns(false)
+    assert.equal(tile.structure.cost, structureType.cost)
 
-    sfactory.checkMoney = checkMoneyStub
+    assert(addStructureSpy.calledWith(tile.structure))
+    assert(buyLandSpy.calledWith(tile))
+    assert(createPollutionSpy.calledWith(structureType.pollution, tile))
+    assert(calcSizeSpy.calledWith(tile.structure))
+  })
+
+  it('Build building does not do anything if checkMoney returns false', () => {
+    mockMethodsForBuildBuildingTests()
+    checkMoneyStub.withArgs(structureType).returns(false)
     sfactory.buildBuilding(tile, structureType)
+
     assert.equal(tile.structure, undefined)
+    assert.equal(createProducerSpy.callCount, 0)
+    assert.equal(addStructureSpy.callCount, 0)
+    assert.equal(buyLandSpy.callCount, 0)
+    assert.equal(createPollutionSpy.callCount, 0)
+    assert.equal(calcSizeSpy.callCount, 0)
   })
 
   it('Money checking works', () =>{
@@ -77,5 +111,11 @@ describe('StructureFactory tests', () => {
     st = {cost: 97}
     assert(sfactory.checkMoney(st), true)
     assert.equal(player.cash, 114)
+  })
+
+  it('buyLandInRadiusForTileOwnership works', () =>{
+    tile.structure = {
+      radiusForTileOwnership: 63
+    }
   })
 })
