57711e90ab6520a09be6deb6f09987a7412f00f2
durak <itapkos@gmail.com>
2017-07-24 15:45:53 +0300

    talteen refineria

--- src/models/map/structure/producers/Refiner.js
+++ src/models/map/structure/producers/Refiner.js
@@ -5,7 +5,7 @@ import Producer from './Producer'
  * Refiner takes production from given type buildingsnear it and enchancts their production
  */
 export default class Refiner extends Producer {
-  constructor ({ zone, inputTypes, multiplier, radius, tile }) {
+  constructor({ zone, inputTypes, multiplier, radius, tile }) {
     super()
 
     this.tile = tile
@@ -20,6 +20,19 @@ export default class Refiner extends Producer {
     this.producerHolders = []
   }
 
+  findProducers () {
+    for (var capsule in this.zone) {
+      let structure = capsule.tile.structure
+      let producer = structure.producer
+      let distance = capsule.distance
+
+      if (this.canRefineOutputOf(structure) && this.isCloser(producer, distance)) {
+        this.producerHolders.push({ distance: distance, producer: producer })
+      }
+    }
+  }
+
+
   produce (timeEvent) {
     var productionSum = 0
     for (let producerHolder of this.producerHolders) {
@@ -27,5 +40,21 @@ export default class Refiner extends Producer {
     }
 
     return Math.round(productionSum * this.multiplier)
+  }
+
+  canRefineOutputOf (structure) {
+    return this.inputTypes.includes(structure.structureType)
+  }
+
+  isCloser (producer, distance) {
+    if (producer.refineryDistance > distance) {
+      producer.refinery = this
+      producer.refineryDistance = distance
+      return true
+    }
+  }
+
+  structureCreated(tile) {
+    
   }
 }
