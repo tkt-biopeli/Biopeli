000f8262a573762ed53ba92b1b0ab45ed2d6e056
Miiu <miia.ramo@helsinki.fi>
2017-07-27 12:05:37 +0300

    testing

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -21,8 +21,8 @@ module.exports = {
 
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: './dist/',
+    path: path.resolve(__dirname, 'deploydist'),
+    publicPath: './deploydist/',
     filename: 'bundle.js'
   },
   plugins: [
