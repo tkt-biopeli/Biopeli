a2a039a033eb48937d3be7d57a00c96dfbdb0d1f
Miiu <miia.ramo@helsinki.fi>
2017-07-31 13:23:07 +0300

    Funktioiden käytön aloitus

--- src/models/structure/producers/decorators/PrimaryProducerDecorator.js
+++ src/models/structure/producers/decorators/PrimaryProducerDecorator.js
@@ -1,4 +1,5 @@
 import config from '../../../../config'
+import Functions from '../../../logic/Functions'
 /**
  * Base class for producers. Producers determine what amount of
  * turnips a structure produces at given week
@@ -8,6 +9,7 @@ export default class PrimaryProducerDecorator {
     this.tile = tile
     this.producer = producer
     this.ownedFarmLand = []
+    this.underFunction = createLine(0.5, 1, 1, increaseAtOne)
   }
 
   produce (timeEvent) {
@@ -15,8 +17,8 @@ export default class PrimaryProducerDecorator {
     this.ownedFarmLand.forEach(function (tile) {
       value += tile.flowers / config.maxFlowers
     }, this)
-    if (tile.moisture < 20) {
-      value = 0
+    if (this.tile.moisture < 20 || this.tile.moisture > 50) {
+      value = value / 2
     }
     return this.producer.produce(timeEvent) * value
   }
