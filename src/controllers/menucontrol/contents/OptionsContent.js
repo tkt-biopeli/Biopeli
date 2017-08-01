faf6427ccb6e6b1677ba83bc93146c88d732a762
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2017-08-01 17:29:17 +0300

    äänen voimakkuuden säätö

--- src/controllers/menucontrol/contents/OptionsContent.js
+++ src/controllers/menucontrol/contents/OptionsContent.js
@@ -16,23 +16,23 @@ export default class OptionsContent extends Content {
   }
 
   incVolume() {
-    if (this.music.volume < 0.9 ) { this.music.volume += 0.1 } 
-    if (this.music.volume > 0.9) { this.music.volume = 1 }
+    if (this.music.volume < 1 ) { this.music.volume += 0.1 } 
+    if (this.music.volume > 1) { this.music.volume = 1 }
   }
 
   decVolume() {
-    if (this.music.volume > 0.1) { this.music.volume -= 0.1 }
-    if (this.music.volume < 0.1) { this.music.volume = 0.1 }
+    if (this.music.volume > 0) { this.music.volume -= 0.1 }
+    if (this.music.volume < 0) { this.music.volume = 0 }
   }
 
   createSections () {
     this.sectionName('options')
-    console.log(this.music.volume)
     if (this.music.mute) {
       this.button('Äänet päälle', this.musicOn, this)
     } else {
       this.button('Äänet pois', this.musicOff, this)  
     }
+    this.text('Äänen voimakkuus: ' + Math.round(this.music.volume*100) + '%')
     this.button('Volyymi +', this.incVolume, this) 
     this.button('Volyymi -', this.decVolume, this)
     this.button('Lopeta', this.gameEvents.finishGame, this.gameEvents,)
