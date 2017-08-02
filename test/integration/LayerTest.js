81ed8ed1fce0bf8ac9e086ca3e2e5bed8d531c43
Miiu <miia.ramo@helsinki.fi>
2017-08-02 13:16:09 +0300

    aloitus

---
+++ test/integration/LayerTest.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+
+describe('Integration test: Layers for moisture and fertility', () => {
+
+
+  beforeEach(() => {
+
+  })
+
+  it('Check something', () => {
+
+  })
+})
