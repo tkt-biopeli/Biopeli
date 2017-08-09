6a064eda56b7232c6661a3d7b95fa8bf5fe987fb
Jarkko Karttunen <jarkko.karttunen1@gmail.com>
2017-08-09 18:31:32 +0300

    .

--- src/utils.js
+++ src/utils.js
@@ -20,5 +20,14 @@ export default {
     xhr.open("POST", config.scoreServer + '/submit_score', true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify(body));
+  },
+
+  fetchScores: () => { // apua tänne jotain järkevää
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", config.scoreServer + 'haloo jotain tähän', false)
+    //xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.send()
+    
+    return xhr.responseText
   }
 }
