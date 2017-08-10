a8303b1c3e610b69d7189553c8a8d10eb9ecd3b7
jon-erikklint <jonerik.klint@gmail.com>
2017-08-10 12:58:58 +0300

    alku

---
+++ src/controllers/events/random/RandomEventCreator.js
@@ -0,0 +1,11 @@
+export default class RandomEventCreator {
+  createEvents (eventJSON) {
+    for (let event of eventJSON) {
+      this.createEvent(event)
+    }
+  }
+
+  createEvent (eventBlueprint) {
+
+  }
+}
