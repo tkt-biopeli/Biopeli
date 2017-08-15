cd93c8b9e04ad0babaa45257e7ce9e900363b1f8
jon-erikklint <jonerik.klint@gmail.com>
2017-08-15 15:43:04 +0300

    testattu

--- src/controllers/events/random/effects/RuiningEffect.js
+++ src/controllers/events/random/effects/RuiningEffect.js
@@ -1,7 +1,8 @@
-import TileEffect from './Effect'
+import Effect from './Effect'
 
-export default class TileValueEffect extends Effect {
+export default class RuiningEffect extends Effect {
   constructor ({json}) {
+    super()
     this.percentage = json.percentage
   }
 
