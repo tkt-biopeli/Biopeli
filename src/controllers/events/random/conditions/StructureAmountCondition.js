d45252cb3bf5f08335e85e21774402b298ff4a64
jon-erikklint <jonerik.klint@gmail.com>
2017-08-16 16:47:13 +0300

    vika ehto ja filter

---
+++ src/controllers/events/random/conditions/StructureAmountCondition.js
@@ -0,0 +1,15 @@
+import {between} from '../../../../models/logic/Between'
+import RecursiveHelper from '../filters/common/RecursiveFilter'
+
+export default class StructureAmountCondition {
+  constructor ({gameState, json}) {
+    var rec = new RecursiveHelper(gameState)
+    this.filter = rec.getFilter(json.structureFilter)
+    this.min = json.min
+    this.max = json.max
+  }
+
+  canHappen () {
+    return between(this.min, this.max, this.filter.affected().length)
+  }
+}
