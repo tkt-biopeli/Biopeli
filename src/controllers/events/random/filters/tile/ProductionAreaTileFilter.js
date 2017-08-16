442c97c68273709e41d605d99339a1b220115c7e
jon-erikklint <jonerik.klint@gmail.com>
2017-08-16 16:51:42 +0300

    eslint

--- src/controllers/events/random/filters/tile/ProductionAreaTileFilter.js
+++ src/controllers/events/random/filters/tile/ProductionAreaTileFilter.js
@@ -1,8 +1,8 @@
 import TileFilter from './TileFilter'
 import RecursiveFilter from '../common/RecursiveFilter'
 
-export default class ProductionAreaTileFilter extends TileFilter{
-  constructor ({gameState, json}) {
+export default class ProductionAreaTileFilter extends TileFilter {
+  constructor ({ gameState, json }) {
     super(gameState)
     var recHelp = new RecursiveFilter(gameState)
 
@@ -16,11 +16,11 @@ export default class ProductionAreaTileFilter extends TileFilter{
   }
 
   isValidTile (tile) {
-    if(this.includeNotOwned && tile.owner == null) return true
-    if(this.structures.length == 0) return false
+    if (this.includeNotOwned && tile.owner == null) return true
+    if (this.structures.length === 0) return false
 
     for (let structure of this.structures) {
-      if(tile.owner == structure) return true
+      if (tile.owner === structure) return true
     }
 
     return false
