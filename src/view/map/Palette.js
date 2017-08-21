c51956de0796d1a35b163c7e9144d8509dbf9c21
Jarkko Karttunen <jarkko.karttunen1@gmail.com>
2017-08-21 14:59:33 +0300

    new colors for fertility

--- src/view/map/Palette.js
+++ src/view/map/Palette.js
@@ -5,7 +5,7 @@ export default class Palette {
     this.dampnessRainbow = new Rainbow()
     this.dampnessRainbow.setSpectrum('sienna', 'sandybrown', 'aqua') 
     this.fertilityRainbow = new Rainbow()
-    this.fertilityRainbow.setSpectrum('red', 'dimgray', 'lime')
+    this.fertilityRainbow.setSpectrum('darkgreen','olive', 'navajowhite')
     this.randomRainbow = new Rainbow()
   }
 
