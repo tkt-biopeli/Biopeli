c2c93a4a1ca836cad9db1da9d3229d8e66295477
Ilja Häkkinen <ilja.hakkinen@helsinki.fi>
2017-08-23 12:52:32 +0300

    phaser-input added to webpack.production.config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,6 +6,7 @@ var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/')
 var phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
 var pixi = path.join(phaserModule, 'build/custom/pixi.js')
 var p2 = path.join(phaserModule, 'build/custom/p2.js')
+var phaserInput = path.join(__dirname, 'node_modules/@orange-games/phaser-input/build/phaser-input.js')
 
 var definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'false'))
@@ -42,7 +43,8 @@ module.exports = {
       { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
       { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
       { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
-      { test: /p2\.js/, use: ['expose-loader?p2'] }
+      { test: /p2\.js/, use: ['expose-loader?p2'] },
+      { test: /phaser\-input\.js$/, use: 'exports-loader?PhaserInput=PhaserInput' }
     ]
   },
   node: {
@@ -54,7 +56,8 @@ module.exports = {
     alias: {
       'phaser': phaser,
       'pixi': pixi,
-      'p2': p2
+      'p2': p2,
+      'phaser-input': phaserInput
     }
   }
 }
