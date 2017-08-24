86b6c02ea74253a65939e9c0108806bdf1264b60
jon-erikklint <jonerik.klint@gmail.com>
2017-08-24 16:03:28 +0300

    yksikkötestit korjattu

--- test/models/structure/StructureFactoryTest.js
+++ test/models/structure/StructureFactoryTest.js
@@ -103,7 +103,6 @@ describe('StructureFactory tests', () => {
     sfactory.producerFactory.createProducer = createProducerSpy
     sfactory.buyLand = buyLandSpy
     sfactory.createInitialPollution = createPollutionSpy
-    sfactory.calculateSize = calcSizeSpy
   }
 
   it('Build building works', () => {
@@ -114,7 +113,6 @@ describe('StructureFactory tests', () => {
     assert.equal(tile.structure.tile, tile)
     assert.equal(tile.structure.ownerName, 'joku omistaja')
     assert.equal(tile.structure.structureName, 'joku rakennus')
-    assert.equal(tile.structure.size, 0)
     assert.equal(tile.structure.structureType, structureType)
     assert.equal(tile.structure.foundingYear, 7)
     assert.equal(1, initializeSpy.callCount)
@@ -122,7 +120,6 @@ describe('StructureFactory tests', () => {
     assert(addStructureSpy.calledWith(tile.structure))
     assert(buyLandSpy.calledWith(tile.structure))
     assert(createPollutionSpy.calledWith(structureType.pollution, tile))
-    assert(calcSizeSpy.calledWith(tile.structure))
     assert.equal(eventController.event.callCount, 1)
   })
 
@@ -153,8 +150,7 @@ describe('StructureFactory tests', () => {
 
   it('LandCanChangeOwnership returns correct booleans with refineries', () => {
     let newOwnerRefinery = {
-      structureType: { type: 'refinery' },
-      takesOwnershipOf: ['grass']
+      structureType: { type: 'refinery', takesOwnershipOf: ['grass']},
     }
     let wrongTiletype = createTmpTile(null, { name: 'foo' }, null, 0)
     let alreadyHasStructure = createTmpTile('another_structure', { name: 'grass' }, null, 0)
@@ -169,8 +165,7 @@ describe('StructureFactory tests', () => {
 
     it('LandCanChangeOwnership returns correct booleans with producers', () => {
     let newOwnerProducer = {
-      structureType: { type: 'producer_structure' },
-      takesOwnershipOf: ['grass']
+      structureType: { type: 'producer_structure', takesOwnershipOf: ['grass'] }
     }
     let wrongTiletype = createTmpTile(null, { name: 'foo' }, null, 0)
     let alreadyHasStructure = createTmpTile('another_structure', { name: 'grass' }, null, 0)
@@ -201,13 +196,13 @@ describe('StructureFactory tests', () => {
 
   it('setNewTileType is functioning properly', () =>{
     var tileType = { name: 'foo' }
-    var structure = {farmland: 'field'}
+    var structure = {structureType: {farmland: 'field'}}
     var tmpTile = createTmpTile(null, tileType, null, 0)
 
     assert.equal(tmpTile.tileType.name, 'foo')
     sfactory.setNewTileType (tmpTile, structure)
     assert.equal(tmpTile.tileType.name, 'field')
-    structure = {farmland: 'grass'}
+    structure.structureType.farmland = 'grass'
     sfactory.setNewTileType (tmpTile, structure)
     assert.equal(tmpTile.tileType.name, 'grass')
   })
