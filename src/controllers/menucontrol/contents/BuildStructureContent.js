7e3ff3cf6465e060e8d5b97677a847b6dfc29548
jon-erikklint <jonerik.klint@gmail.com>
2017-08-25 13:09:11 +0300

    tietoa perustuotannosta

--- src/controllers/menucontrol/contents/BuildStructureContent.js
+++ src/controllers/menucontrol/contents/BuildStructureContent.js
@@ -22,31 +22,79 @@ export default class BuildStructureContent extends Content {
 
   createPreBuildInfoContent (stype) {
     this.text(this.texts.structureType + ': ' + stype.nameWithLanguage)
+    this.text(this.texts.health + ': ' + stype.health)
+    this.text(this.texts.owningRadius + ': ' + stype.radiusForTileOwnership)
+    
+    this.addPollutionInfo(stype)
     this.addCategorySpecificInfo(stype)
+    
     this.text(this.texts.cost + ': ' + stype.cost, 'large')
+  }
+  
+  addPollutionInfo (stype) {
+    var pollution = stype.pollution
+
+    let pollutionInfo = ''
+    if (pollution.constant) {
+      pollutionInfo += this.texts.constantPollution
+    }else {
+      pollutionInfo += this.texts.pilePollution
+    }
+
+    this.text(pollutionInfo)
+    this.text(this.texts.pollutionRadius + ': ' + pollution.distance)
+    this.text(this.texts.pollutionAmount + ': ' + pollution.amount)
   }
 
   addCategorySpecificInfo (stype) {
     switch (stype.type) {
       case 'producer_structure':
-        if (stype.continuousProduction) {
-          this.text(this.texts.continuousProduction)
-        } else {
-          var text = this.texts.harvest + ': '
-          for (let harvest of stype.harvestingWeeks) {
-            text += harvest
-          }
-          this.text(text)
-        }
-
-        this.text(this.texts.revenue + ': ' + stype.turnipYield)
+        this.primaryProducerInfo(stype)
         break
       case 'refinery':
-        //
+        this.refineryInfo(stype)
+        break
+      case 'special':
+        this.specialInfo(stype)
         break
       default:
-        return null
+        this.errorInfo(stype)
     }
+  }
+
+  primaryProducerInfo (stype) {
+    if (stype.continuousProduction) {
+      this.text(this.texts.continuousProduction)
+    } else {
+      var harvests = this.texts.harvest + ': '
+      for (let harvest of stype.harvestingWeeks) {
+        harvests += harvest
+      }
+      this.text(harvests)
+    }
+
+    let ownershipText = this.texts.takesOwnership + ': '
+    for (let i = 0 ; i < stype.takesOwnershipOf.length ; i++) {
+      ownershipText += stype.takesOwnershipOf[i]
+      if(i != stype.takesOwnershipOf.length -1) ownershipText += ', '
+    }
+
+    this.text(ownershipText)
+    this.text(this.texts.revenue + ': ' + stype.turnipYield)
+    this.text(this.texts.fertility + ': ' + stype.fertilityMin + '->' + stype.fertilityMax)
+    this.text(this.texts.moisture + ': ' + stype.moistureMin + '->' + stype.moistureMax)
+  }
+
+  refineryInfo (stype) {
+
+  }
+
+  specialInfo (stype) {
+
+  }
+
+  errorInfo (stype) {
+    this.text(this.texts.errorType)
   }
 
   createBuildContent (stype, tile) {
