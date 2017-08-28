a1cff78944c94c9a5a4a232aaaaecf9aaa644234
Miiu <miia.ramo@helsinki.fi>
2017-08-28 22:02:31 +0300

    Vinkkejä ja event lisätty

--- src/controllers/events/random/conditions/StructureAmountCondition.js
+++ src/controllers/events/random/conditions/StructureAmountCondition.js
@@ -3,13 +3,8 @@ import FilterGetterComponent from '../filters/components/FilterGetterComponent'
 
 export default class StructureAmountCondition {
   constructor ({gameState, json}) {
-<<<<<<< HEAD
-    var rec = new RecursiveHelper(gameState)
-    this.filter = rec.getFilter(json.filter)
-=======
     var filterGetter = new FilterGetterComponent({gameState: gameState})
     this.filter = filterGetter.getFilter(json.structureFilter)
->>>>>>> 44bb173eed8deed10450381c540b558f93e8a1a4
     this.min = json.min
     this.max = json.max
   }
