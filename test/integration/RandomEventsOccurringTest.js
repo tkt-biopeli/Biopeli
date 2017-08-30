7a815716c8e0e97f600eb15c7c84b319dd2590b4
Ilja Häkkinen <ilja.hakkinen@cs.helsinki.fi>
2017-08-30 13:49:06 +0300

    fix for an integration test

--- test/integration/RandomEventsOccurringTest.js
+++ test/integration/RandomEventsOccurringTest.js
@@ -14,14 +14,15 @@ describe('Integration test: Random event occurrence tests', () => {
 
   it('Random event does not occur if its conditions are not met', () => {
     // random events do not occur
+    gameAdvancer.setPopulation(100)
     gameAdvancer.updateSeveralTimes(5, 1000)
     gameStateChecker.checkRandomEventOccurred('Pakolaiskriisi', false)
     // the condition of the first event is not met;
     // the last event occurs (one with no conditions)
     gameAdvancer.activateRandomEvents()
     gameAdvancer.update(1000)
-    //gameStateChecker.checkRandomEventOccurred('Pakolaiskriisi', true) 
-    //gameStateChecker.checkRandomEventOccurred('Rutto', false)
+    gameStateChecker.checkRandomEventOccurred('Pakolaiskriisi', true) 
+    gameStateChecker.checkRandomEventOccurred('Rutto', false)
   })
 
   it('Random event occurs when its conditions are met', () => {
